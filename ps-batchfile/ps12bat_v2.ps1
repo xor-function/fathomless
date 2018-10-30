cba59e2ee62539dc0538ec8d4456d4429085a661
xor-function <xor-function@users.noreply.github.com>
2018-10-30 14:32:57 -0500

    Updated formatting

--- ps-batchfile/ps12bat_v2.ps1
+++ ps-batchfile/ps12bat_v2.ps1
@@ -220,7 +220,7 @@ function genenc-script {
 	[int]$loopccnt = '0'
 	
 	foreach ($char in $charArray)
-    {
+	{
 		$loopccnt++;$lineccnt++;$loop++	
 		if ( [int]$lineccnt -eq '1' ) { $cmdfile += 'echo | set /p="' + $char
 		} else {$cmdfile += $char }
